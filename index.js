74bd2cbb9e3266f80e0cf6b16972102aa221cd9e
Rouan Thompson <rouan.thompson@gmail.com>
2020-08-03 02:39:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const array = []
+    for(let i = 0; i < names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return array;
+}
+
+function countDown(num){
+
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+} 
